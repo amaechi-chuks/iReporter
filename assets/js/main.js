caec771c273dbe7879814b6d31e3784083a34944
chuks_nlog <amaechichuks2000@yahoo.com>
2018-11-20 20:57:12 +0100

    feature: create red-flag user page

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,23 @@ const loadIntroTexts = () => {
 }
 window.onload = loadIntroTexts();
 
+/**
+ * @method loadSideBar responsible for the behaviour of the menu on mobile
+ * @description loads hamburger menu once the window loads
+ */
+app.loadSideBar = () => {
+    const btns = Array.from(document.getElementsByClassName('js__navbar__toggler'));
+    const navBarCollaspe = document.querySelector('.navbar__collapse');
+  
+    btns.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (navBarCollaspe.style.display === 'none') {
+          navBarCollaspe.style.display = 'block';
+        } else {
+          navBarCollaspe.style.display = 'none';
+        }
+      });
+    });
+  };
+  
