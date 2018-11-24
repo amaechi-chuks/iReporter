44f2bf3946c76ff8006c05efa4184be51f3fb6fc
chuks_nlog <amaechichuks2000@yahoo.com>
2018-11-24 20:34:52 +0100

    feature:create a GET api for all red-flag

--- server/routes/incidentRoutes.js
+++ server/routes/incidentRoutes.js
@@ -7,9 +7,14 @@ const { IncidentController } = controllers;
 
 const router = express.Router();
 
-
+//route to create a red-flag record
 router.route('/red-flags')
 .post((req, res) => IncidentController.createIncident(req, res, 'Red Flag'));
+
+//route to get all red-flag record
+
+router.route('/red-flags')
+.get((req, res) => IncidentController.getAllIncident(req, res, 'Red Flag'));
 
 
 export default router;
