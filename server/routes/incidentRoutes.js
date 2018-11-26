4448b1cbf14280f1e6799daad2ec79c9681b82a6
chuks_nlog <amaechichuks2000@yahoo.com>
2018-11-27 00:04:11 +0100

    feature: create api endpoint to edit redlag location

--- server/routes/incidentRoutes.js
+++ server/routes/incidentRoutes.js
@@ -10,14 +10,40 @@ const router = express.Router();
 router.route('/red-flags')
     .post(IncidentController.createIncident);
 
+//create a intervention record
+router.route('/intervention')
+    .post(IncidentController.createIncident);
+
 //Get all red-flag record
 router.route('/red-flags')
-      .get(IncidentController.getAllIncident);
+    .get(IncidentController.getAllIncident);
+
+//Get all red-flag record
+router.route('/intervention')
+    .get(IncidentController.getAllIncident);
 
 
 //Get red-flag by id
 router.route('/red-flag/:id')
     .get(IncidentController.getSingleRedFlag);
 
+//Get intervention by id
+router.route('/intervention/:id')
+    .get(IncidentController.getSingleRedFlag);
 
+//Update redFlag by location   
+router.route('/red-flag/:id/location')
+    .put(IncidentController.updateRedFlagByLocation);
+
+//Update intervention by location 
+router.route('/intervention/:id/location')
+    .put(IncidentController.updateRedFlagByLocation);
+
+// //Update red-flag comment
+// router.route('/red-flag/:id/comment')
+//     .put(IncidentController.updateRedFlagByComment);
+
+// //Update intervention comment
+// router.route('/intervention/:id/comment')
+//     .put(IncidentController.updateRedFlagByComment);
 export default router;
